dc687319ee40a25ce01500fe52b595e846f80333
christian <106384215+Theplanter@users.noreply.github.com>
2024-09-11 14:53:18 -0400

    Add buttons to HTML, EventListener to change color of div when hovered over it

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
 const container = document.querySelector(".container")
+let stat = true
 
+function filler(Telement, state){
 
-for(let i = 0;i<10;i++){
-const col = document.createElement("div")
-col.classList.add("col")
-
-    for(let i = 0;i<10;i++){
     const row = document.createElement("div")
     row.classList.add("row")
-    col.appendChild(row)
+    row.id = "row"
+
+    const red = Math.random()*255
+    const green = Math.random()*255
+    const blue = Math.random()*255
+
+    //when the user hover over the div it changes to black
+    row.addEventListener("mouseenter", function(){
+     
+this.style.backgroundColor = "black"
+        
+    });
+//when the row changes color it get updated to the columns
+    Telement.appendChild(row)
 }
-container.appendChild(col)
-
-}
-
-// const col = document.querySelector(".row")
 
 
 
-// }
+function canvasSize(size){
+//the size gets passed through the for loop making columns
+
+    for(let i = 0;i<size;i++){
+
+        const col = document.createElement("div")
+        col.classList.add("col")
+
+        
+        for(let i = 0;i<size;i++){
+            filler(col, stat)
+        
+    }
+    container.appendChild(col)
+
+
+}}
+
+canvasSize(15)
